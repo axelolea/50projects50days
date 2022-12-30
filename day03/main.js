3148f3da8cd086b3f7518174a61e8f97b698f1df
axel olea <axelcedilloolea@gmail.com>
2022-12-30 05:35:26 -0600

    Day 3 Finally

--- day03/main.js
+++ day03/main.js
@@ -1,13 +1,16 @@
 const btn = document.getElementById("button");
 const content = document.getElementById("container");
+const items = document.querySelectorAll(".list-item");
 
 btn.addEventListener("click", () => {
   const currentState = btn.getAttribute("closed");
   if (currentState === "false") {
     btn.setAttribute("closed", "true");
     content.classList.add('show-nav')
+    items.forEach((item) => item.classList.add('active'));
   } else {
     btn.setAttribute("closed", "false");
-    content.classList.remove('show-nav')
+    content.classList.remove('show-nav');
+    items.forEach((item) => item.classList.remove('active'));
   }
 });
