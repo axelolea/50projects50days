a3bd432d8bef984fda4cd732b440cfdcf15bed1b
axel olea <axelcedilloolea@gmail.com>
2023-01-01 15:24:23 -0600

    Day 6 Finally

---
+++ day06/main.js
@@ -0,0 +1,15 @@
+const boxes = document.querySelectorAll('.content-box')
+const activedArea = 7/10
+
+window.addEventListener('scroll', () => {
+    const triggerBotton = window.innerHeight * activedArea
+    boxes.forEach( box => {
+        const boxTop = box.getBoundingClientRect().top
+        if(triggerBotton > boxTop) {
+            box.classList.add('show');
+        }
+        else {
+            box.classList.remove('show');
+        }
+    })
+})
