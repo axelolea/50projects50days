ac954c28f1f8ee579dae13bdbdc3a2f77da9ef24
Axel Olea <axelcedilloolea@gmail.com>
2023-01-10 07:33:16 -0600

    Finally Day 14 & Fix day 12

--- day14/main.js
+++ day14/main.js
@@ -1,8 +1,18 @@
 const btn = document.getElementById('navbar-button')
 const navbar = document.getElementById('navbar')
-
+const icons = document.querySelectorAll('.bi')
 btn.addEventListener('click', () => {
     btn.firstElementChild.classList.toggle('active')
     navbar.classList.toggle('active')
 })
 
+icons.forEach(icon => {
+    icon.addEventListener('mouseover', () => {
+        const text = document.querySelector(`.${icon.id}`)
+        text.classList.add('active')
+    })
+    icon.addEventListener('mouseout', () => {
+        const text = document.querySelector(`.${icon.id}`)
+        text.classList.remove('active')
+    })
+})
--- day15/main.js
+++ day15/main.js
@@ -2,8 +2,8 @@ const counters = document.querySelectorAll('.count-number')
 
 const updateCounter = (count) => {
     const target = count.getAttribute('data-target')
-    const timeInterval = 50
-    const maxIterations = 62
+    const timeInterval = 10
+    const maxIterations = 310
     var value = 0
     const setValue = () => {
         if(value > maxIterations){
@@ -11,7 +11,7 @@ const updateCounter = (count) => {
             count.innerText = target
             return
         }
-        const valueFunc = ((Math.sin((value * 0.05) - (Math.PI / 2)) + 1) / 2) * target
+        const valueFunc = ((Math.sin((value * 0.01) - (Math.PI / 2)) + 1) / 2) * target
         count.innerText = `${Math.ceil(valueFunc)}`
         value++
     }
